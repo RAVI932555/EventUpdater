cb6846757e6d3b8836e7fea3001369573addabfd
unknown <baraderavi4@gmail.com>
2025-11-15 21:18:27 +0530

    added frontend and backend project structure

---
+++ EventUpdater-backend/src/main/java/com/eventUpdater/EventUpdaterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventUpdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventUpdaterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventUpdaterBackendApplication.class, args);
+	}
+
+}
